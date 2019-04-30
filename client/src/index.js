c88efc385a83625e80b4fad9b51cf800dc50ac2f
Ayush Upneja <upneja@bu.edu>
2019-04-30 15:55:28 -0400

    adding loading screen for index

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Preloader, Placeholder } from 'react-preloading-screen';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
